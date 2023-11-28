a101a2e81cd5e8ebea67b1910f65d891431686ea
Usama Rahman <rahmanusama390@gmail.com>
2023-11-29 04:32:09 +0600

    Start :)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <h1 className="">The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
