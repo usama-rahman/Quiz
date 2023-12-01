ef478cefed26122756bc8a0d512ce6a80047ce8d
Usama Rahman <rahmanusama390@gmail.com>
2023-12-01 21:55:33 +0600

    Working

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
+import questions from "./Questions";
+
 export default function Play() {
+  const [currentQuestion, setcurrentQuestion] = useState(0);
+
+  const handelNextBtnClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    setcurrentQuestion(nextQuestion);
+  };
   return (
-    <div className="w-4/5 flex justify-center">
-      <div className="w-full ml-9 items-center  ">
-        <input
-          type="range"
-          className=" w-full cursor-pointer"
-          min={0}
-          max={15}
-        />
+    <div className=" ">
+      <div className="flex justify-between">
+        <h3>
+          Question <span className="font-bold"> {currentQuestion + 1}</span> /
+          {questions.length}
+        </h3>
+        <h3>
+          <span className="font-bold"> 1</span> / 280 Points
+        </h3>
+      </div>
+      <div>
+        <h3 className="my-6 ">
+          <span className="font-bold">
+            {questions[currentQuestion].question}
+          </span>
+        </h3>
+        {questions[currentQuestion].options.map((answerOption) => (
+          // eslint-disable-next-line react/jsx-key
+          <button className="my-2 w-full hover:bg-yellow-800 ">
+            {answerOption}
+          </button>
+        ))}
+      </div>
+
+      <div className="flex justify-between mt-8">
+        <button>7:23</button>
+        <button onClick={handelNextBtnClick}>Next</button>
       </div>
     </div>
   );
