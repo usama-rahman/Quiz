6f57a6827bad6c393f314e947141794e37164482
Usama Rahman <rahmanusama390@gmail.com>
2023-12-01 06:26:15 +0600

    @

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,7 +1,14 @@
 export default function Play() {
   return (
-    <div>
-      <input type="text" />
+    <div className="w-4/5 flex justify-center">
+      <div className="w-full ml-9 items-center  ">
+        <input
+          type="range"
+          className=" w-full cursor-pointer"
+          min={0}
+          max={15}
+        />
+      </div>
     </div>
   );
 }
