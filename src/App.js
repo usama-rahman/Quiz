cbdb21af2fbf7088d24bb4d140f6e31bdcdd2a5a
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-01 21:26:34 +0600

    06 Reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-// import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  question: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecevied":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecevied", payload: data }))
+      .catch((err) => dispatch({ type: "error" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
