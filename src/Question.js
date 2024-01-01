774dda5362358a8de868b98d8868471ad220d594
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-01 23:18:40 +0600

    Error

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,10 @@
-const Question = () => {
-  return <div>Question</div>;
+const Question = ([question]) => {
+  console.log(question);
+  return (
+    <div>
+      <h4> question.question </h4>
+    </div>
+  );
 };
 
 export default Question;
